debf0945b4da5f881fe78d59eb78cd649a810f94
coulthard <928861960@qq.com>
2019-01-22 16:01:45 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 // 引入状态管理
 import Store from "./store/index";
+// 引入路由
+import Router from "./router";
 // 引入element ui
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
@@ -9,8 +11,9 @@ import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
-console.log(Store);
+
 new Vue({
   store: Store,
+  router: Router,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// use
+
+Vue.use(VueRouter);
+
+const routers = [
+  // 登陆界面
+  {
+    path: "/login",
+    meta: { title: "登陆" },
+    component: resolve => require(["./views/login.vue"], resolve)
+  }
+];
+
+export default new VueRouter({
+  mode: "history",
+  routes: routers
+});
